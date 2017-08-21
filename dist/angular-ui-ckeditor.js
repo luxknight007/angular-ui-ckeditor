af20629a02a93124df7c457049dae0a0ad5f0ffd
Zoilo Dela Cruz <luxknight007@gmail.com>
2017-08-21 20:47:02 +0800

    Add document auto generation

---
+++ dist/angular-ui-ckeditor.js
@@ -0,0 +1,68 @@
+(function() {
+    'use strict';
+
+    angular.module('angular-ui-ckeditor', []);
+  }());
+
+(function() {
+    'use strict';
+
+    angular.module('angular-ui-ckeditor', [])
+        .directive('uiCkeditor', UiCkeditorDirective)
+        .controller('uiCkeditorCtrl', uiCkeditorCtrl);
+
+    UiCkeditorDirective.$inject = [];
+
+    function UiCkeditorDirective() {
+        return {
+            restrict: 'A',
+            controller: 'uiCkeditorCtrl',
+            controllerAs: '$ctrl',
+            link: Linker,
+            replace: true,
+            require: ['ui-ckeditor', 'ngModel'],
+            scope: {
+                data: '='
+            }
+        };
+    }
+
+    function Linker(scope, element, attrs, ctrls) {
+        var uiCkeditorCtrl = ctrls[0];
+        var ngModelController = ctrls[1];
+
+        uiCkeditorCtrl.instance.on('change', function syncView() {
+            ngModelController.$setViewValue(uiCkeditorCtrl.instance.getData());
+        });
+
+        function init() {
+            uiCkeditorCtrl.instance.setData(scope.data);
+        }
+
+        init();
+    }
+
+    uiCkeditorCtrl.$inject = ['$scope', '$window', '$element', '$attrs', '$parse'];
+
+    function uiCkeditorCtrl($scope, $window, $element, $attrs, $parse) {
+        var ctrl = this;
+        ctrl.instance = {};
+        // Parse and get the configuration define in the parent
+        ctrl.config = $parse($attrs.ckeditor)($scope.$parent) || {};
+        ctrl.editorElement = $element[0];
+        ctrl.loaded = false;
+
+        angular.extend(ctrl, {
+            ckeditor: $window.CKEDITOR
+        });
+
+        function init() {
+            ctrl.instance = ctrl.editorElement.hasAttribute('contenteditable') &&
+            ctrl.editorElement.getAttribute('contenteditable').toLowerCase() === 'true' ?
+                ctrl.ckeditor.inline(ctrl.editorElement, ctrl.config) :
+                ctrl.ckeditor.replace(ctrl.editorElement, ctrl.config);
+        }
+
+        init();
+    }
+}());
---
+++ gulpfile.js
@@ -0,0 +1,147 @@
+/* jshint node: true */
+
+'use strict';
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var concat = require('gulp-concat');
+var replace = require('gulp-replace');
+var eslint = require('gulp-eslint');
+var Server = require('karma').Server;
+var path = require('path');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var rm = require('gulp-rm');
+var ghpages = require('gulp-gh-pages');
+var cp = require('child_process');
+var coveralls = require('gulp-coveralls');
+
+var version = require('./package.json').version;
+
+// Command line option:
+//  --fatal=[warning|error|off]
+var fatalLevel = require('yargs').argv.fatal || 'error';
+
+var ERROR_LEVELS = ['error', 'warning'];
+
+function handleError(level, error) {
+    gutil.log(error.message);
+    if ( ERROR_LEVELS.indexOf(level) <= ERROR_LEVELS.indexOf(fatalLevel) ) {
+        process.exit(1);
+    }
+}
+
+gulp.task('lint', function() {
+    return gulp.src([
+        './gulpfile.js',
+        './src/**/*.js'
+    ])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+        .on('error', function(err) {
+            handleError('warning', err);
+            this.emit('end');
+        });
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        './src/angular-ui-ckeditor.js',
+        './src/**/*.js'
+    ])
+        .pipe(replace('@version', version))
+        .pipe(concat('angular-ui-ckeditor.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('angular-ui-ckeditor.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+/* gulp.task('styles', function() {
+    return gulp.src('./src/styles/!*.less')
+        .pipe(concat('ml-search-ng-tpls.less'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('ml-search-ng-tpls.css'))
+        .pipe(less())
+        .pipe(gulp.dest('dist'));
+});*/
+
+/* gulp.task('templates', function() {
+    return gulp.src([ './src/!**!/!*.html' ])
+        .pipe(minifyHtml({
+            empty: true,
+            spare: true,
+            quotes: true
+        }))
+        // TODO: ? prefix: '/ml-search'
+        .pipe(html2Js({
+            moduleName: 'ml.search.tpls',
+            prefix: '/'
+        }))
+        .pipe(concat('ml-search-ng-tpls.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('ml-search-ng-tpls.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});*/
+
+function karmaResult(cb, result) {
+    var err = null;
+    if (result === 1) {
+        err = new Error('test failed');
+    }
+    cb(err);
+}
+
+gulp.task('test', ['lint'], function(done) {
+    new Server({
+        configFile: path.join(__dirname, './karma.conf.js'),
+        singleRun: true,
+        autoWatch: false
+    }, karmaResult.bind(null, done))
+        .start();
+});
+
+gulp.task('autotest', function(done) {
+    new Server({
+        configFile: path.join(__dirname, './karma.conf.js'),
+        autoWatch: true
+    }, karmaResult.bind(null, done))
+        .start();
+});
+
+gulp.task('docs', function(done) {
+    cp.exec('./node_modules/.bin/jsdoc -c jsdoc.conf.json', function(err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        gulp.src([
+            './docs/generated/css/baseline.css',
+            './docs/custom-styles.css'
+        ])
+            .pipe(concat('baseline.css'))
+            .pipe(gulp.dest('./docs/generated/css'))
+            .on('end', function() {
+                done();
+            });
+    });
+});
+
+gulp.task('clean-docs', function() {
+    return gulp.src('./docs/generated/**/*', { read: false })
+        .pipe(rm({async: false}));
+});
+
+gulp.task('publish-docs', function() {
+    return gulp.src([ './docs/generated/**/*.*' ])
+        .pipe(ghpages());
+});
+
+gulp.task('coveralls', function() {
+    return gulp.src('coverage/**/lcov.info')
+        .pipe(coveralls());
+});
+
+gulp.task('default', ['lint', 'test', 'scripts']);
